3da9f988ddd38415c4079e2ee291aece2488430f
SIEV Kakada <92452767+Kakada10@users.noreply.github.com>
2024-07-20 13:47:57 +0700

    Update hero.tsx

--- components/landing/hero.tsx
+++ components/landing/hero.tsx
@@ -37,7 +37,7 @@ export default function HeroLanding() {
             SUPERCHARGE
           </Text>
           <br />
-          Your Notion Docs for PROD v1
+          Your Notion Docs for PROD v2
         </Heading>
         <Text color={"gray.500"} fontSize={"xl"}>
           Welcome to Wotion: a library of dynamic widgets for Notion.
